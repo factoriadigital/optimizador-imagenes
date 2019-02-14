081613d80f2a43998a7f8e60a3451748de170204
Toni Cortés <tonicc77@gmail.com>
2019-02-14 13:37:15 +0000

    Add optimization for png files

--- optimize.sh
+++ optimize.sh
@@ -31,7 +31,6 @@ function optimize()
     echo -e "\nStarting optimization process..."
     echo -e "\n--------------------------------"
     convert_cmd
-    optipng_cmd
     echo -e "\nFixing permissions..."
     chown -R $OWNER:$OWNER $ROOT_DIR
     echo -e "\nCOMPLETED!"
@@ -42,12 +41,6 @@ function convert_cmd()
 {
     echo -e "\nConvert CMD in progress..."
     find $ROOT_DIR -type f -iname '*.jpg' -exec convert -quality 60 -strip \{} \{} \;
-}
-
-function optipng_cmd()
-{
-    echo -e "\nOptiPNG CMD in progress..."
-    find $ROOT_DIR -type f -iname '*.png' -exec optipng -o7 -strip all {} \;
 }
 
 optimize
